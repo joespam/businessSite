ab9653ffea16c1b3df976e6aeea2050188b9fb06
Joe Spampinato <jcspampinato@gmail.com>
2015-10-23 15:57:30 -0400

    Initial commit

---
+++ public/js/action.js
@@ -0,0 +1,28 @@
+// 
+//  Javascript - JCS Week 2 Day 1 final jQuery Exercise
+// 
+
+//
+//  commented out as a stub for future use
+//
+
+// define function to be run later
+var whenReady = function () {
+
+	// this is our callback function
+	var showAgain = function() {
+			// $('subscribe-login').hide()
+	};
+
+	var onclick = function() {
+		// $('subscribe-login').show()
+	}
+
+	$('provide').click( provideClick );		
+	// $('subscribe').click( subscribeClick );
+};
+
+// bind function to ready event
+$(document).ready(whenReady);
+
+
